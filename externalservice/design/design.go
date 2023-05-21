ab7591fd5c8349f71b7b4c6c1fce1af94f073c55
lokesh <lokesh@asu.edu>
2023-05-21 15:11:00 -0700

    adds CRUD for Character

--- externalservice/design/design.go
+++ externalservice/design/design.go
@@ -13,3 +13,82 @@ var _ = API("externalservice", func() {
 		})
 	})
 })
+
+var Character = Type("Character", func() {
+	Description("Character in the Game")
+	Field(1, "id", Int, "Character ID")
+	Field(2, "name", String, "Character name")
+	Field(3, "description", String, "Character description")
+	Field(4, "health", Int, "Character health")
+	Field(5, "experience", Int, "Character experience")
+})
+
+var _ = Service("externalservice", func() {
+	Description("Contains API CRUD operations on characters, inventories, and items.")
+
+	// Creating character with unique name and required payload
+	Method("create_character", func() {
+		Description("Creating a new Character")
+		Payload(func() {
+			Field(1, "name", String, "Character name")
+			Field(2, "description", String, "Character description")
+			Field(3, "health", Int, "Character health")
+			Field(4, "experience", Int, "Character experience")
+			Required("name", "health", "experience")
+		})
+		Result(Character)
+		Error("bad_request", String, "Bad request")
+		Error("unique_constraint", String, "Name not unique")
+		HTTP(func() {
+			POST("/characters")
+			Response(StatusCreated)
+		})
+	})
+
+	// Get a character by ID
+	Method("get_character", func() {
+		Description("Fetching a Character")
+		Payload(func() {
+			Field(1, "id", Int, "Character ID")
+			Required("id")
+		})
+		Result(Character)
+		Error("not_found", String, "Character not found")
+		HTTP(func() {
+			GET("/characters/{id}")
+			Response(StatusOK)
+		})
+	})
+
+	// Updating a Character by ID
+	Method("update_character", func() {
+		Description("Updating a Character")
+		Payload(func() {
+			Field(1, "id", Int, "Character ID")
+			Field(2, "name", String, "Character name")
+			Field(3, "description", String, "Character description")
+			Field(4, "health", Int, "Character health")
+			Field(5, "experience", Int, "Character experience")
+			Required("id")
+		})
+		Error("not_found", String, "Character not found")
+		HTTP(func() {
+			PUT("/characters/{id}")
+			Response(StatusOK)
+		})
+	})
+
+	// Deleting a Character by ID
+	Method("update_character", func() {
+		Description("Deleting a Character")
+		Payload(func() {
+			Field(1, "id", Int, "Character ID")
+			Required("id")
+		})
+		Error("not_found", String, "Character not found")
+		HTTP(func() {
+			DELETE("/characters/{id}")
+			Response(StatusOK)
+		})
+	})
+})
