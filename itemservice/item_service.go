63841055fe03bd545e331870d426d4e45e5702e0
lokesh <lokesh@asu.edu>
2023-05-23 20:39:05 -0700

    removes comment from item_Service

--- itemservice/item_service.go
+++ itemservice/item_service.go
@@ -6,8 +6,6 @@ import (
 	"log"
 )
 
-// ItemService service example implementation.
-// The example methods log the requests and return zero values.
 type itemServicesrvc struct {
 	logger           *log.Logger
 	index            int
@@ -15,21 +13,20 @@ type itemServicesrvc struct {
 	uniqueNameMapper map[string]bool
 }
 
-// NewItemService returns the ItemService service implementation.
 func NewItemService(logger *log.Logger) itemservice.Service {
-	return &itemServicesrvc{logger: logger,
+	return &itemServicesrvc{
+		logger:           logger,
+		index:            0,
 		idItemMapper:     make(map[int]*itemservice.Item),
-		uniqueNameMapper: make(map[string]bool)}
+		uniqueNameMapper: make(map[string]bool),
+	}
 }
 
-// Create a new item
 func (s *itemServicesrvc) Create(ctx context.Context, p *itemservice.CreatePayload) (res int, err error) {
-	s.logger.Print("itemService.Create")
 	if _, ok := s.uniqueNameMapper[p.Name]; ok {
 		return 0, itemservice.UniqueConstraint("Name not unique")
 	}
-	// TODO: To store INDEX in DB
-	s.index += 1
+	s.index++
 	currentIndex := s.index
 	item := &itemservice.Item{
 		ID:          &currentIndex,
@@ -44,10 +41,7 @@ func (s *itemServicesrvc) Create(ctx context.Context, p *itemservice.CreatePaylo
 	return s.index, nil
 }
 
-// Retrieve an item by ID
 func (s *itemServicesrvc) Get(ctx context.Context, p *itemservice.GetPayload) (res *itemservice.Item, err error) {
-
-	s.logger.Print("itemService.Get")
 	res, ok := s.idItemMapper[p.ID]
 	if !ok {
 		return nil, itemservice.NotFound("Not Found")
@@ -55,9 +49,7 @@ func (s *itemServicesrvc) Get(ctx context.Context, p *itemservice.GetPayload) (r
 	return res, nil
 }
 
-// Update an item
 func (s *itemServicesrvc) Update(ctx context.Context, p *itemservice.UpdatePayload) (err error) {
-	s.logger.Print("itemService.Update")
 	res, ok := s.idItemMapper[p.ID]
 	if !ok {
 		return itemservice.NotFound("Not Found")
@@ -68,7 +60,7 @@ func (s *itemServicesrvc) Update(ctx context.Context, p *itemservice.UpdatePaylo
 		}
 		delete(s.uniqueNameMapper, *res.Name)
 	}
-	character := &itemservice.Item{
+	item := &itemservice.Item{
 		ID:          res.ID,
 		Name:        p.Name,
 		Description: p.Description,
@@ -76,14 +68,12 @@ func (s *itemServicesrvc) Update(ctx context.Context, p *itemservice.UpdatePaylo
 		Healing:     p.Healing,
 		Protection:  p.Protection,
 	}
-	s.idItemMapper[p.ID] = character
+	s.idItemMapper[p.ID] = item
 	s.uniqueNameMapper[*p.Name] = true
 	return nil
 }
 
-// Delete an item by ID
 func (s *itemServicesrvc) Delete(ctx context.Context, p *itemservice.DeletePayload) (err error) {
-	s.logger.Print("itemService.Delete")
 	res, ok := s.idItemMapper[p.ID]
 	if !ok {
 		return itemservice.NotFound("Not Found")
